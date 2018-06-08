85184186c6b7e27b2b405f2da4df8a58f0e25a0f
Stanislav <staskalyuk.096@gmail.com>
2018-06-08 17:30:41 +0300

    Example and finished biird client

---
+++ Biird_Client/Assets/Biird/BiirdClient.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Biird
+{
+	public class BiirdClient : MonoBehaviour
+	{
+		public delegate void TextReceived(string text);
+		public event TextReceived OnTextReceived;
+
+		private const string Url = "https://api.biird.io/resourceValue/";
+
+		private string _selectedLanguage = "en";
+	
+
+		#region Setup Biird
+
+		/// <summary>
+		/// Get instance of Biird class
+		/// </summary>
+		/// <returns>Biird object</returns>
+		public static BiirdClient GetBiird()
+		{
+			var biird = FindObjectOfType<BiirdClient>();
+			if (biird == null)
+			{
+				var gameObject = new GameObject("Biird");
+				biird = gameObject.AddComponent<BiirdClient>();
+			}
+			return biird;
+		}
+		
+		/// <summary>
+		/// Set current language
+		/// </summary>
+		/// <exception cref="ArgumentNullException"> Throws when use unsupported langauage</exception>
+		/// <param name="language"></param>
+		public void SelectLanguage(SupportedLanguages language)
+		{
+			switch (language)
+			{
+				case SupportedLanguages.English:
+					_selectedLanguage = "en";
+					break;
+				case SupportedLanguages.German:
+					_selectedLanguage = "de";
+					break;
+				case SupportedLanguages.Franch:
+					_selectedLanguage = "fr";
+					break;
+				default:
+					throw new ArgumentNullException();
+			}
+		}
+
+		#endregion
+		
+
+		#region Download Data
+
+		/// <summary>
+		/// Start process to receive text with id
+		/// </summary>
+		/// <exception cref="ArgumentNullException"> Throws when id is empty or null</exception>
+		/// <param name="id">item id</param>
+		public void ReceiveTheText(string id)
+		{
+			if (string.IsNullOrEmpty(id))
+			{
+				throw new ArgumentNullException();
+			}
+			StartCoroutine(GetText(id));
+		}
+		
+		private IEnumerator GetText(string id)
+		{
+			string result;
+			using (var request = UnityWebRequest.Get(Url + id + "?language=" + _selectedLanguage))
+			{
+				yield return request.SendWebRequest();
+
+				if (request.isNetworkError || request.isHttpError)
+				{
+					Debug.LogError(request.error);
+					yield break;
+				}
+				result = request.downloadHandler.text;
+				Debug.Log("Receive: " + result);
+			}
+
+			if (OnTextReceived != null)
+			{
+				OnTextReceived(result);
+			}
+		}
+
+		#endregion
+		
+	}
+
+	public enum SupportedLanguages : byte
+	{
+		English, German, Franch
+	}
+
+	
+}
---
+++ Biird_Client/Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+﻿using Biird;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+	private BiirdClient _biird;
+	[SerializeField] private Text _testText;
+	
+	private void Awake()
+	{
+		_biird = BiirdClient.GetBiird();
+		_biird.OnTextReceived+=OnTextReceived;
+		_biird.SelectLanguage(SupportedLanguages.English);
+		_biird.ReceiveTheText("b9fb0f44-31d5-45df-9ec3-776568802c31"); //<p>Hello World!</p>
+	}
+
+	private void OnTextReceived(string text)
+	{
+		_testText.text = text;
+	}
+}
